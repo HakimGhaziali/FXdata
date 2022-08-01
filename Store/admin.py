b46475d963e6ee599132332ab86212ef994d0a74
hakimghaziali@gmail.com <hakimghaziali@gmail.com>
2022-08-01 17:55:21 +0430

    create content models that have two ForeigKey

--- Store/admin.py
+++ Store/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-
-# Register your models here.
-
-
-from .models import Book
-
-class Display(admin.ModelAdmin):
-
-    list_display = ['title','author']
+from .models import Book , Review
 
 
+class ReviewInline(admin.TabularInline):
 
-admin.site.register(Book,Display)
+    model = Review
+
+
+class BookAdmin(admin.ModelAdmin):
+
+    inlines = [ ReviewInline , ]
+    list_display = ('title', 'author', 'price')
+    
+admin.site.register(Book, BookAdmin)
--- Store/models.py
+++ Store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.urls import reverse
-
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -17,3 +17,14 @@ class Book(models.Model):
     
 
 
+
+class Review(models.Model):
+
+    content = models.TextField()
+    author = models.ForeignKey(get_user_model() , on_delete= models.CASCADE, related_name='reviews')
+    Book= models.ForeignKey(Book , related_name ='reviews' , on_delete= models.CASCADE)
+
+
+    def __str__(self):
+
+        return self.content
