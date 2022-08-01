dbaee429163bb2bb4232e0de3fdf51d53d1dc503
hakimghaziali@gmail.com <hakimghaziali@gmail.com>
2022-08-01 12:08:57 +0430

    start store of my website

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'blog',
     'crispy_forms',
+    'Store',
 
 ]
 
--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),  # new
     path("accounts/", include("django.contrib.auth.urls")),
     path("blog/", include("blog.urls")),  # new
+    path("store/", include("store.urls")),  # new
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
