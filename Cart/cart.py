66878366e33d86d90123ecd5043e13106bf2d3cf
HakimGh <hakimghaziali@gmail.com>
2022-09-04 21:06:46 +0430

    create cart app

---
+++ Cart/cart.py
@@ -0,0 +1,63 @@
+
+from Store.models import Book
+
+class Cart:
+
+    def __init__(self, request):
+
+        self.request = request
+        self.session = request.session
+
+        cart = self.session.get('cart')
+
+        if not cart:
+            cart =self.session['cart'] = {}
+            
+        
+        self.cart = cart
+
+    
+    def add(self , product , quantity=1 ):
+
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+
+            self.cart[product_id] = {'quantity': quantity}
+        else:
+            self.cart[product_id]['quantity'] +=quantity
+
+        self.save()
+
+    
+    def remove(self,product):
+
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+
+            del self.cart[product_id]
+            self.save()
+
+    def save(self):
+
+        self.session.modified = True
+
+    
+    def __iter__(self):
+
+        product_ids = self.cart.keys()
+        products = Book.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+
+
+
+
+
+
+
+        
+
+
+        
--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'blog',
     'crispy_forms',
     'Store',
+    'Cart'
 
     #package
     
